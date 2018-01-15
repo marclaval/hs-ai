7e4c242bb5a9f08b6a750af0b0c7fe88939c87af
Marc Laval <marc.laval.opensource@gmail.com>
2018-01-15 22:07:45 +0100

    feat: add beginner decks for all classes

--- src/Deck/Decks.cs
+++ src/Deck/Decks.cs
@@ -4,8 +4,17 @@ using SabberStoneCore.Enums;
 
 namespace HSAI.Deck
 {
-	public class Beginner
+    // Toast's beginner decks: https://disguisedtoast.com/articles/261-new-and-beginner-players
+    public class Beginner
 	{
         public static string Druid = "AAECAZICAtUBoAUOzQG3BpYNQPsEtAJa4ASeCeQI2AGKB5oI/gEA";
-	}
+        public static string Hunter = "AAECAR8PqAK7BdkJ2gqkAusHgQK1A9kK2AGXCN4EiAWNAe0GAA==";
+        public static string Mage = "AAECAf0ED+wHlg2LA/sETbQCuwJa4ASeCasElgXYAYoHtAQA";
+        public static string Paladin = "AAECAZ8FD/EFlg3wA88G+wTcA1qvB+AEgQL5CtgBigeIBUYA";
+        public static string Priest = "AAECAa0GAgjjBw6WDckGxgT7BFrgBNMKngnXCtgBigflBPIMlwIA";
+        public static string Rogue = "AAECAaIHBvEF9gSlA+MHzQOgBQyWDdkC+wS0AlrgBJ4JmwXYAYoHywO0AQA=";
+        public static string Shaman = "AAECAaoID70B8AOTCfsEiwRa4ASBAvkK/gXvAfAH2AGKB4gFAA==";
+        public static string Warlock = "AAECAf0GAoAGhQMO+weWDcYE+wTjB7YHWuAEkgeeCdgBigfOB8QIAA==";
+        public static string Warrior = "AAECAQcCkQb8BA6WDbAC+wQctAJa/wfgBJ4J7weRA6wH2AGKBwA=";
+    }
 }
