187293266bdaec48b826ae6177bb5d5634c06527
Marc Laval <marc.laval.opensource@gmail.com>
2018-02-03 00:27:53 +0100

    chore: refactor dependencies

--- init_dependencies.sh
+++ init_dependencies.sh
@@ -2,5 +2,14 @@
 
 set -e -o pipefail
 
-git clone https://github.com/HearthSim/SabberStone.git ./external_projects/SabberStone
+mkdir -p external_projects
+
+git clone https://github.com/marclaval/SabberStone.git ./external_projects/SabberStone
+cd external_projects/SabberStone
+git checkout RandomController
+cd ../..
+
 git clone https://github.com/HearthSim/Hearthstone-Deck-Tracker.git ./external_projects/Hearthstone-Deck-Tracker
+cd external_projects/Hearthstone-Deck-Tracker
+cmd "/C bootstrap.bat"
+cd ../..
