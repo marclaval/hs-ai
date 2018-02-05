30d9ce9518934ba3a0a5be4eb4da09715aa510f9
Marc Laval <marc.laval.opensource@gmail.com>
2018-02-05 16:17:11 +0100

    chore: switch to fork of Hearthstone-Deck-Tracker

--- init_dependencies.sh
+++ init_dependencies.sh
@@ -9,7 +9,8 @@ cd external_projects/SabberStone
 git checkout RandomController
 cd ../..
 
-git clone https://github.com/HearthSim/Hearthstone-Deck-Tracker.git ./external_projects/Hearthstone-Deck-Tracker
+git clone https://github.com/marclaval/Hearthstone-Deck-Tracker.git ./external_projects/Hearthstone-Deck-Tracker
 cd external_projects/Hearthstone-Deck-Tracker
+git checkout moreEventHandlers
 cmd "/C bootstrap.bat"
 cd ../..
